7198a25a90292a0a4126698570f9ebda999edb6f
vzwGrey <vzwgrey@gmail.com>
2021-12-02 10:58:33 +0100

    feat(day2): solutions for day2

---
+++ src/day2.rs
@@ -0,0 +1,69 @@
+enum Command {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+fn parse_input(input: &'_ str) -> impl Iterator<Item = Command> + '_ {
+    input.split('\n').filter(|s| !s.trim().is_empty()).map(|s| {
+        let mut parts = s.split(' ');
+        let cmd = parts
+            .next()
+            .expect("each command should have a command word");
+        let dis = parts
+            .next()
+            .expect("each command should have a distance")
+            .parse()
+            .expect("each command should have a distance following it");
+        match cmd {
+            "forward" => Command::Forward(dis),
+            "down" => Command::Down(dis),
+            "up" => Command::Up(dis),
+            _ => panic!("invalid command"),
+        }
+    })
+}
+
+pub fn part1(input: &str) -> i32 {
+    let (hor, depth) = parse_input(input).fold((0, 0), |(hor, depth), cmd| match cmd {
+        Command::Forward(dis) => (hor + dis, depth),
+        Command::Down(dis) => (hor, depth + dis),
+        Command::Up(dis) => (hor, depth - dis),
+    });
+    hor * depth
+}
+
+pub fn part2(input: &str) -> i32 {
+    let (hor, depth, _) = parse_input(input).fold((0, 0, 0), |(hor, depth, aim), cmd| match cmd {
+        Command::Forward(dis) => (hor + dis, depth + dis * aim, aim),
+        Command::Down(dis) => (hor, depth, aim + dis),
+        Command::Up(dis) => (hor, depth, aim - dis),
+    });
+    hor * depth
+}
+
+#[test]
+fn test_part1() {
+    let input = r#"
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+    "#;
+    assert_eq!(part1(input), 150);
+}
+
+#[test]
+fn test_part2() {
+    let input = r#"
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+    "#;
+    assert_eq!(part2(input), 900);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 mod day1;
+mod day2;
 
 use std::{fs, io};
 
+type SolutionFn = for<'r> fn(&'r str) -> i32;
+type Solution = (SolutionFn, SolutionFn);
+
 fn main() -> io::Result<()> {
-    let solutions = [(day1::part1, day1::part2)];
+    let solutions: [Solution; 2] = [(day1::part1, day1::part2), (day2::part1, day2::part2)];
 
     for (i, (part1, part2)) in solutions.iter().enumerate() {
-        let input = fs::read_to_string("./data/day1.txt")?;
-        println!("Day {}: ", i + 1);
+        let day = i + 1;
+        let input = fs::read_to_string(format!("./data/day{}.txt", day))?;
+        println!("Day {}: ", day);
         println!("  Part 1: {}", part1(&input));
         println!("  Part 2: {}", part2(&input));
     }
